07a5d3e35f56fdf4a23bc4b16c032feeb2e553e0
Abderrahmane Bouzar <abderrahmane.bouzar@outlook.fr>
2022-09-29 00:29:58 +0100

    add mobile design add js script

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 const subbtn = document.querySelector(".button");
-const FormControlList = [...document.getElementsByClassName("formControl")];
-
+const formcontrol = document.querySelector(".formControl");
+console.log(formcontrol);
 removeErrors();
 //Remove Errors
 function removeErrors() {
-    for (let i = 0; i < FormControlList.length; i++) {
-        FormControlList[i].querySelector('img').classList.add('hide');
-        FormControlList[i].querySelector('small').classList.add('hide');
-    }
+
+    formcontrol.querySelector('img').classList.add('hide');
+    formcontrol.querySelector('small').classList.add('hide');
+
 }
 
 subbtn.addEventListener("click", subbtnfun);
 
 function subbtnfun() {
-    for (let i = 0; i < FormControlList.length; i++) {
+    if(!validate(formcontrol.querySelector('.Email').value)){
+        console.log("false");
+        formcontrol.querySelector('img').classList.remove('hide');
+        formcontrol.querySelector('small').classList.remove('hide');
+    }else{
 
-        checkField(FormControlList[i], i);
-
-
+        formcontrol.querySelector('img').classList.add('hide');
+        formcontrol.querySelector('small').classList.add('hide');
     }
 }
 
@@ -27,43 +30,4 @@ function validate(email) {
     return re.test(String(email).toLowerCase());
 
 }
-function checkIfEmpty(str) {
-    console.log(str.value);
-    if (str.value.trim().length === 0) {
-        return true;
-    }
-    return false;
-}
-function checkField(field, i) {
-    if (i != 2) {
-        if (!checkIfEmpty(field.querySelector('input'))) {
-            removeErrorClass(field);
-        } else {
-            addErrorClass(field);
-        }
 
-    } else {
-        if (validate(field.querySelector('input').value)) {
-            removeErrorClass(field);
-        } else {
-            addErrorClass(field);
-        }
-    }
-
-}
-
-function addErrorClass(field) {
-    field.querySelector('small').classList.remove('hide');
-    field.querySelector('img').classList.remove('hide');
-    field.querySelector('small').classList.add('show');
-    field.querySelector('img').classList.add('show');
-    field.querySelector('input').classList.add('error');
-}
-
-function removeErrorClass(field) {
-    field.querySelector('small').classList.remove('show');
-    field.querySelector('img').classList.remove('show');
-    field.querySelector('input').classList.remove('error');
-    field.querySelector('img').classList.add('hide');
-    field.querySelector('small').classList.add('hide');
-}
